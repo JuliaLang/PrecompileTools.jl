1cccfae6c1279d61d83f51b974dcac5d7fdc8e7f
Tim Holy <tim.holy@gmail.com>
2025-03-18 13:19:36 -0500

    Update src/PrecompileTools.jl
    Co-authored-by: Jameson Nash <vtjnash@gmail.com>

--- src/PrecompileTools.jl
+++ src/PrecompileTools.jl
@@ -7,7 +7,7 @@ export @setup_workload, @compile_workload, @recompile_invalidations
 const verbose = Ref(false)    # if true, prints all the precompiles
 
 function precompile_mi(mi::Core.MethodInstance)
-    precompile(mi.specTypes) # TODO: Julia should allow one to pass `mi` directly (would handle `invoke` properly)
+    precompile(mi)
     verbose[] && println(mi)
     return
 end
