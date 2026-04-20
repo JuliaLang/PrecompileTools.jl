1fb75dc96c18f40fabe9726e32c08beb7910f6d2
Kristoffer Carlsson <kcarlsson89@gmail.com>
2026-04-20 14:31:05 +0200

    record if precompile tools is fully disabled at precompile time so its cachefile invalidates properly when changed

--- src/PrecompileTools.jl
+++ src/PrecompileTools.jl
@@ -5,6 +5,7 @@ using Preferences
 export @setup_workload, @compile_workload, @recompile_invalidations
 
 const verbose = Ref(false)    # if true, prints all the precompiles
+const enabled = @load_preference("precompile_workloads", true)::Bool
 
 function precompile_mi(mi::Core.MethodInstance)
     precompile(mi)
